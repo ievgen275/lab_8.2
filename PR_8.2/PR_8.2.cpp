d629978471c02764f50c7e8a08cb317f080f41c6
ievgen275 <ievgen3435@gmail.com>
2024-03-14 16:23:00 +0200

    first commit

---
+++ PR_8.2/PR_8.2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int CountWords(const char* s)
+{
+    int wordCount = 0;
+    bool inWord = false;
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\r')
+        {
+            inWord = false;
+        }
+        else
+        {
+            if (!inWord)
+            {
+                wordCount++;
+                inWord = true;
+            }
+        }
+    }
+
+    return wordCount;
+}
+
+int main()
+{
+    const int MAX_LENGTH = 100;
+    char s[MAX_LENGTH + 1];
+
+    cout << "Enter string:" << endl;
+    cin.getline(s, MAX_LENGTH);
+
+    cout << "The string has " << CountWords(s) << " words" << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_8.2/PR_8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const char* s = "";
+			Assert::AreEqual(0, CountWords(s));
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			const char* s = "hello    world";
+			Assert::AreEqual(2, CountWords(s));
+		}
+	};
+}
